cfb9b7d0a056a8e54b71238631324f4ccd1ee077
yugecin <yugecin@users.noreply.github.com>
2016-09-27 19:22:08 +0200

    add moverfactories

---
+++ src/yugecin/opsudance/movers/factories/HalfLowEllipseMoverFactory.java
@@ -0,0 +1,33 @@
+/*
+ * opsu!dance - fork of opsu! with cursordance auto
+ * Copyright (C) 2016 yugecin
+ *
+ * opsu!dance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opsu! is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with opsu!dance.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package yugecin.opsudance.movers.factories;
+
+import itdelatrisu.opsu.objects.GameObject;
+import yugecin.opsudance.movers.HalfEllipseMover;
+import yugecin.opsudance.movers.Mover;
+
+public class HalfLowEllipseMoverFactory implements MoverFactory {
+
+	@Override
+	public Mover create(GameObject start, GameObject end, int dir) {
+		HalfEllipseMover m = new HalfEllipseMover(start, end, dir);
+		m.setMod(0.5d);
+		return m;
+	}
+
+}
