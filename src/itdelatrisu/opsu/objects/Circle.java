eb696077a2ee32d6c5818c0d767e66cf1021c9fe
yugecin <yugecin@users.noreply.github.com>
2016-09-27 21:29:03 +0200

    we want to dance

--- src/itdelatrisu/opsu/objects/Circle.java
+++ src/itdelatrisu/opsu/objects/Circle.java
@@ -38,7 +38,7 @@ import org.newdawn.slick.Graphics;
  */
 public class Circle extends GameObject {
 	/** The diameter of hit circles. */
-	private static float diameter;
+	public static float diameter;
 
 	/** The associated HitObject. */
 	private HitObject hitObject;
