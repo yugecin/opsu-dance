c7538ef348b15be7df0eeac5ad15db6cfce78c99
yugecin <yugecin@users.noreply.github.com>
2016-10-01 12:06:05 +0200

    Add 1000fps option

--- src/itdelatrisu/opsu/Options.java
+++ src/itdelatrisu/opsu/Options.java
@@ -1261,7 +1261,7 @@ public class Options {
 	private static Skin skin;
 
 	/** Frame limiters. */
-	private static final int[] targetFPS = { 60, 120, 240 };
+	private static final int[] targetFPS = { 60, 120, 240, 1000 };
 
 	/** Index in targetFPS[] array. */
 	private static int targetFPSindex = 0;
