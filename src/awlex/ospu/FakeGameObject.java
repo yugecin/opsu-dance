515fea06372ae628935af417b15b943dab53c559
Awlex <wieseralex580@gmail.com>
2016-10-11 14:45:58 +0200

    Added new Movers, Factories and Spinner

---
+++ src/awlex/ospu/FakeGameObject.java
@@ -0,0 +1,104 @@
+package awlex.ospu;
+
+/**
+ * Created by Awlex on 10.10.2016.
+ */
+
+import itdelatrisu.opsu.Options;
+import itdelatrisu.opsu.objects.GameObject;
+import itdelatrisu.opsu.objects.curves.Vec2f;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+/**
+ * This class is just a dummy {@link GameObject} to place in the middle of 2 GameObjects.
+ * Might as well ignore it
+ */
+public class FakeGameObject extends GameObject {
+
+    //Time between the 2 constructor objects
+    private int halfTime;
+
+    public FakeGameObject() {
+        this.start = new Vec2f();
+        this.end = new Vec2f();
+        this.start.x = this.end.x = Options.width / 2;
+        this.start.y = this.end.y = Options.height / 2;
+    }
+
+    public FakeGameObject(GameObject start, GameObject end) {
+        halfTime = start.getEndTime() + (end.getTime() - start.getEndTime()) / 2;
+        this.start = new Vec2f();
+        this.end = new Vec2f();
+        this.start.x = this.end.x = (start.end.x + end.start.x) / 2;
+        this.start.y = this.end.y = (start.end.y + end.start.y) / 2;
+    }
+
+    @Override
+    public void draw(Graphics g, int trackPosition, boolean mirrored) {
+
+    }
+
+    @Override
+    public boolean update(boolean overlap, int delta, int mouseX, int mouseY, boolean keyPressed, int trackPosition) {
+        return false;
+    }
+
+    @Override
+    public boolean mousePressed(int x, int y, int trackPosition) {
+        return false;
+    }
+
+    @Override
+    public Vec2f getPointAt(int trackPosition) {
+        return null;
+    }
+
+    @Override
+    public int getEndTime() {
+        return halfTime;
+    }
+
+    @Override
+    public int getTime() {
+        return halfTime;
+    }
+
+    @Override
+    public void updatePosition() {
+
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public boolean isCircle() {
+        return false;
+    }
+
+    @Override
+    public boolean isSlider() {
+        return false;
+    }
+
+    @Override
+    public boolean isSpinner() {
+        return false;
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public Color getMirroredColor() {
+        return null;
+    }
+
+    public void setTime(int time) {
+        this.halfTime = time;
+    }
+}
---
+++ src/awlex/ospu/movers/factories/SpiralMoverFactory.java
@@ -0,0 +1,97 @@
+package awlex.ospu.movers.factories;
+
+import itdelatrisu.opsu.Options;
+import itdelatrisu.opsu.Utils;
+import itdelatrisu.opsu.objects.GameObject;
+import awlex.ospu.movers.CentralSpiralMover;
+import awlex.ospu.movers.CombinedSpiralMover;
+import yugecin.opsudance.movers.Mover;
+import awlex.ospu.movers.SpiralToMover;
+import yugecin.opsudance.movers.factories.MoverFactory;
+
+/**
+ * Created by Alex Wieser on 09.10.2016.
+ * WHO DO YOU THINK I AM?
+ */
+public class SpiralMoverFactory implements MoverFactory {
+
+    private int startTime;
+    private int endTime;
+
+    /**
+     * This method will return either a {@link CentralSpiralMover}, {@link SpiralToMover} or
+     * {@link CombinedSpiralMover}, depending on the situation
+     *
+     * @param start
+     * @param end
+     * @param dir
+     * @return
+     */
+    @Override
+    public Mover create(GameObject start, GameObject end, int dir) {
+
+        startTime = start.getEndTime();
+        endTime = end.getTime();
+
+        SpiralToMover spiralTo = new SpiralToMover(start, end, dir);
+        CentralSpiralMover center = new CentralSpiralMover(start, end, dir);
+
+        if (Utils.distance(start.end.x, start.end.y, end.start.y, end.start.x) > 150) {
+            if (inBounds(spiralTo) || inBounds(spiralTo = new SpiralToMover(start, end, -dir)))
+                return spiralTo;
+            else if (inBounds(center) || inBounds(center = new CentralSpiralMover(start, end, -dir)))
+                return center;
+
+        } else {
+            if (inBounds(center) || inBounds(center = new CentralSpiralMover(start, end, -dir)))
+                return center;
+            else if (inBounds(spiralTo) || inBounds(spiralTo = new SpiralToMover(start, end, -dir)))
+                return spiralTo;
+        }
+        return new CombinedSpiralMover(start, end, dir);
+    }
+
+    /**
+     * Checks if the spiral is in bounds. <br> <br>
+     * TODO: Find a better algorithm and more effizient way to to this, by finding the crucial point in the arithmetic spiral. This also applies for other inBounds-methods
+     * <br><br>This method fails miserably in the beginning and on the maps with big and fast jumps (e.g.
+     *
+     * @param mover
+     * @return
+     */
+    public boolean inBounds(Mover mover) {
+        boolean ret = true;
+        int middle = endTime - startTime;
+        for (int i = 1; ret && i < 15; i++) {
+            ret = checkBounds(mover.getPointAt(startTime + (middle * i) / 16));
+            System.out.println("i: " + i + " = " + ret);
+        }
+
+        return ret;
+        /*return checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .9))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .8))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .7))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .6))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .5))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .4))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .3))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .2))) &&
+                checkBounds(mover.getPointAt((int) (startTime + (endTime - startTime) * .1)));
+                */
+    }
+
+    /**
+     * checks if a point is in bounds
+     *
+     * @param pos
+     * @return
+     */
+    private boolean checkBounds(double[] pos) {
+        return 0 < pos[0] && pos[0] < Options.width && 0 < pos[1] && pos[1] < Options.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Spiral me right round, baby";
+    }
+}
