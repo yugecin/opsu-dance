cb09e2149c7f7cc9e2aa5b1bedf02b7594992057
yugecin <yugecin@users.noreply.github.com>
2016-12-13 15:45:18 +0100

    let sliders shrink one point more for more fluent shrinking

--- src/itdelatrisu/opsu/objects/Slider.java
+++ src/itdelatrisu/opsu/objects/Slider.java
@@ -429,7 +429,7 @@ public class Slider extends GameObject {
 				if (repeats % 2 == 0) {
 					game.spliceSliderCurve(baseSliderFrom + (int) ((1d - curveIntervalFrom) * curvelen) - 1, baseSliderFrom + curvelen);
 				} else {
-					game.setSlidercurveFrom(baseSliderFrom + (int) (curveIntervalFrom * curvelen));
+					game.setSlidercurveFrom(baseSliderFrom + (int) (curveIntervalFrom * curvelen) + 1);
 				}
 			}
 			game.setSlidercurveTo(baseSliderFrom + (int) (curveIntervalTo * curve.getCurvePoints().length));
