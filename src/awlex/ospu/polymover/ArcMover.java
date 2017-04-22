70ef9296717cae06c2039b6447985c25a184cbc9
Awlex <wieseralex580@gmail.com>
2017-04-22 02:52:28 +0200

    Maybe fixed glitching. Can't test

--- src/awlex/ospu/polymover/ArcMover.java
+++ src/awlex/ospu/polymover/ArcMover.java
@@ -15,81 +15,56 @@ public class ArcMover implements PolyMover {
 	public static final int ITEMS_NEEDED = 3;
 	private GameObject p1, middle, p2;
 	private double xm, ym, r, alpha, beta, gamma;
-	
+
 	public ArcMover(GameObject p1, GameObject middle, GameObject p2) {
 		this.p1 = p1;
 		this.middle = middle;
 		this.p2 = p2;
 		init();
 	}
-	
+
 	private void init() {
 		Matrix m = prepareMatrix(p1, middle, p2);
 		xm = m.get(1, 0) * 0.5;
 		ym = m.get(2, 0) * 0.5;
 		r = sqrt(pow(xm, 2) + pow(ym, 2) - m.get(0, 0));
-		alpha = atan2(p1.end.y - ym, p1.end.x - xm);
-		beta = atan2(middle.start.y - ym, middle.start.x - xm);
-		gamma = atan2(p2.start.y - ym, p2.start.x - xm);
+		alpha = (atan2(p1.end.y - ym, p1.end.x - xm) + 360) % 360;
+		beta = (atan2(middle.start.y - ym, middle.start.x - xm) + 360) % 360;
+
+		// Fix angles
+		if (beta - alpha > 180) {
+			beta -= 360;
+		}
+		if (gamma - beta > 180) {
+			gamma -= 180;
+		}
 	}
-	
+
 	@Override
 	public double[] getPointAt(int time) {
 		double angle;
 		if (time < middle.getTime()) {
-			double percent = ((double) time - p1.getEndTime()) / ((middle.getTime() - p1.getEndTime()));
+			double percent = ((double) time - p1.getEndTime()) / (middle.getTime() - p1.getEndTime());
 			angle = alpha + (beta - alpha) * percent;
+		} else {
+			double percent = ((double) time - middle.getTime()) / (p2.getTime() - middle.getTime());
+			angle = beta + (gamma - beta) * percent;
 		}
-		else {
-			angle = beta + (gamma - beta) * ((double) time - middle.getTime()) / (p2.getTime() - middle.getTime());
-		}
-		return new double[]{
-			xm + r * cos(angle),
-			ym + r * sin(angle)
-		};
+		return new double[]{xm + r * cos(angle), ym + r * sin(angle)};
 	}
-	
+
 	@Override
 	public GameObject[] getItems() {
-		return new GameObject[]{
-			p1,
-			middle,
-			p2
-		};
+		return new GameObject[]{p1, middle, p2};
 	}
 
 	private static Matrix prepareMatrix(GameObject p1, GameObject middle, GameObject p2) {
 		Matrix a, b;
-
-		//if (!p2.isSlider()) {
-			a = new Matrix(new double[][]{
-				{1, -p1.end.x, -p1.end.y},
-				{1, -middle.start.x, -middle.start.y},
-				{1, -p2.start.x, -p2.start.y}
-			});
-			b = new Matrix(new double[][]{
-				{-(pow(p1.end.x, 2) + pow(p1.end.y, 2))},
-				{-(pow(middle.start.x, 2) + pow(middle.start.y, 2))},
-				{-(pow(p2.start.x, 2) + pow(p2.start.y, 2))},
-			});
-		/*
-		} else {
-			Circle c = ((Slider) p2).getTickPositionCircles()[0];
-			a = new Matrix(new double[][]{
-				{1, -p1.end.x, -p1.end.y},
-				{1, -middle.start.x, -middle.start.y},
-				{1, -c.start.x, -c.start.y}
-			});
-			b = new Matrix(new double[][]{
-				{-(pow(p1.end.x, 2) + pow(p1.end.y, 2))},
-				{-(pow(middle.start.x, 2) + pow(middle.start.y, 2))},
-				{-(pow(c.start.x, 2) + pow(c.start.y, 2))},
-			});
-		}
-		*/
+		a = new Matrix(new double[][]{{1, -p1.end.x, -p1.end.y}, {1, -middle.start.x, -middle.start.y}, {1, -p2.start.x, -p2.start.y}});
+		b = new Matrix(new double[][]{{-(pow(p1.end.x, 2) + pow(p1.end.y, 2))}, {-(pow(middle.start.x, 2) + pow(middle.start.y, 2))}, {-(pow(p2.start.x, 2) + pow(p2.start.y, 2))},});
 		return a.solve(b);
 	}
-	
+
 	public static boolean canCricleExistBetweenItems(GameObject p1, GameObject p2, GameObject p3) {
 		try {
 			prepareMatrix(p1, p2, p3);
